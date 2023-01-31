a4734af97b2dae33182014e7cc5364d7d2bbd62e
YunjaeGong <llegokid@naver.com>
2023-01-31 18:35:34 +0900

    feat: Session 모델 추가
    https://github.com/softeerbootcamp/Team3/issues/3
    
    유저 식별할 session 생성

---
+++ backend/gathergo/src/main/java/lightning/gathergo/model/Session.java
@@ -0,0 +1,41 @@
+package lightning.gathergo.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class Session {
+    protected String sessionId;
+    protected final LocalDateTime createDate;
+    protected String userId;
+    protected String userName;
+
+    public Session(String userId, String userName) {
+        this.sessionId = String.valueOf(UUID.randomUUID());
+        this.userId = userId;
+        this.userName = userName;
+        this.createDate = LocalDateTime.now();
+    }
+    public Session(String sessionId, String userId, String userName, LocalDateTime createDate) {
+        this.sessionId = sessionId;
+        this.userId = userId;
+        this.userName = userName;
+        this.createDate = createDate;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public LocalDateTime getCreateDate() {
+            return createDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
